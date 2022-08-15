1e527a6d5ff174a3816ef7bafb2c10af4c28af22
kusem <kusem4uk@gmail.com>
2022-08-15 13:24:27 +0300

    Basics hometask done.

---
+++ src/basics/basics.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace basics;
+
+class basics implements BasicsInterface
+{
+    private BasicsValidator $validator;
+
+    public function __construct()
+    {
+        $this->validator = new BasicsValidator();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getMinuteQuarter(int $minute): string
+    {
+        $this->validator->isMinutesException($minute);
+
+        if ($minute > 45 | $minute == 0) {
+            return "fourth";
+        }
+        if ($minute <= 15) {
+            return "first";
+        }
+
+        if ($minute <= 30) {
+            return "second";
+        }
+
+        return "third";
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isLeapYear(int $year): bool
+    {
+        $this->validator->isYearException($year);
+
+        return ((($year % 4) == 0) && ((($year % 100) != 0) || (($year % 400) == 0)));
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isSumEqual(string $input): bool
+    {
+        $this->validator->isValidStringException($input);
+
+        $convertedString = str_split($input);
+        $firstHalf =
+            (int)$convertedString[0] +
+            (int)$convertedString[1] +
+            (int)$convertedString[2];
+        $secondHalf =
+            (int)$convertedString[3] +
+            (int)$convertedString[4] +
+            (int)$convertedString[5];
+
+        return $firstHalf == $secondHalf;
+    }
+}

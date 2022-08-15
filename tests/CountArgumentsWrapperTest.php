a5188979ee7f9959697ff04426ac3d0c9a7ad793
kusem <kusem4uk@gmail.com>
2022-08-15 13:49:25 +0300

    Functions hometask done.

---
+++ tests/CountArgumentsWrapperTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class CountArgumentsWrapperTest extends TestCase
+{
+    protected $functions;
+
+    protected function setUp(): void
+    {
+        $this->functions = new functions\Functions();
+    }
+
+    public function testNegative()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $this->functions->countArgumentsWrapper(2, 4, 0, 2, ['russiaisaterroriststate']);
+    }
+
+}

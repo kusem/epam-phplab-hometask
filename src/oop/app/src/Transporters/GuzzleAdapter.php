9fae20e798d3ae258ddb891948dfdf779a156ef3
kusem <58442387+kusem@users.noreply.github.com>
2022-08-22 18:04:32 +0300

    Update GuzzleAdapter.php

--- src/oop/app/src/Transporters/GuzzleAdapter.php
+++ src/oop/app/src/Transporters/GuzzleAdapter.php
@@ -3,22 +3,27 @@
 namespace src\oop\app\src\Transporters;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
 
 class GuzzleAdapter implements TransportInterface
 {
+    private Client $client;
+
+    public function __construct(){
+        $this->client = new Client();
+    }
     /**
      * @inheritDoc
      */
     public function getContent(string $url): string
     {
-        $client = new Client();
-        $res = $client->request(
+        $res = $this->client->request(
             'GET',
             $url,
             [
                 'headers' => [
                     'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
-                ],
+                ]
             ]
         );
 

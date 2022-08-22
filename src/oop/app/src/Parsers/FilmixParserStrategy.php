d31d48c77b75ee3bbbd0871b43ec1f8f498a92f4
kusem <58442387+kusem@users.noreply.github.com>
2022-08-23 00:33:55 +0300

    Update FilmixParserStrategy.php

--- src/oop/app/src/Parsers/FilmixParserStrategy.php
+++ src/oop/app/src/Parsers/FilmixParserStrategy.php
@@ -6,6 +6,16 @@ use src\oop\app\src\Models\Movie;
 
 class FilmixParserStrategy implements ParserInterface
 {
+    private Movie $movie;
+
+    /**
+     * Generating Movie class just after class init
+     */
+    public function __construct()
+    {
+        $this->movie = new Movie();
+    }
+
     /**
      * Parses link to poster image from original HTML page
      *
@@ -31,11 +41,7 @@ class FilmixParserStrategy implements ParserInterface
     {
         $titleTagBegin = strpos($rawHTML, '<h1 class="name" itemprop="name">');
         $titleTagEnd = strpos($rawHTML, '</h1>', $titleTagBegin);
-        $movieTitle = substr(
-            $rawHTML,
-            $titleTagBegin + strlen('<h1 class="name" itemprop="name">'),
-            $titleTagEnd - $titleTagBegin - strlen('<h1 class="name" itemprop="name">')
-        );
+        $movieTitle = substr($rawHTML, $titleTagBegin + strlen('<h1 class="name" itemprop="name">'), $titleTagEnd - $titleTagBegin-strlen('<h1 class="name" itemprop="name">'));
 
         return $movieTitle;
     }
@@ -50,27 +56,17 @@ class FilmixParserStrategy implements ParserInterface
     {
         $descrTagBegin = strpos($rawHTML, '<div class="full-story">');
         $descrTagEnd = strpos($rawHTML, '</div>', $descrTagBegin);
-        $movieDescr = substr(
-            $rawHTML,
-            $descrTagBegin + strlen('<div class="full-story">'),
-            $descrTagEnd - $descrTagBegin - strlen('<div class="full-story">')
-        );
+        $movieDescr = substr($rawHTML, $descrTagBegin + strlen('<div class="full-story">'), $descrTagEnd - $descrTagBegin-strlen('<div class="full-story">'));
 
         return $movieDescr;
     }
 
-    /**
-     * Takes site content string and returns Movie with title/post/description.
-     *
-     * @param string $siteContent
-     * @return Movie
-     */
-    public function parseContent(string $siteContent): Movie
+    public function parseContent(string $siteContent)
     {
-        $title = $this->getMovieName($siteContent);
-        $poster = $this->getPoster($siteContent);
-        $description = $this->getMovieDescription($siteContent);
+        $this->movie->setTitle($this->getMovieName($siteContent));
+        $this->movie->setPoster($this->getPoster($siteContent));
+        $this->movie->setDescription($this->getMovieDescription($siteContent));
 
-        return new Movie($title, $poster, $description);
+        return $this->movie;
     }
 }
